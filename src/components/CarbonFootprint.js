bc6f6932015f15a311f382be48bb1c4917035b62
aswinpmadhu <pmadhuaswin666@gmail.com>
2021-07-15 22:27:40 +0530

    equivalencies frontend

--- src/components/CarbonFootprint.js
+++ src/components/CarbonFootprint.js
@@ -7,14 +7,19 @@ function CarbonFootprint(props) {
     
     return (
         <div className="c02" >
-            <Popup trigger={<div onMouseOver={(e)=>{e.target.className="red"}} onMouseOut={(e)=>e.target.className=""}> <i class="fas fa-shoe-prints"></i><b> {props.co2.value}</b> of CO2/{props.co2.unit} trip</div>} position="right center">
-                <div>
+            <Popup trigger={<div onMouseOver={(e)=>{e.target.className="red"}} onMouseOut={(e)=>e.target.className=""}> <i class="fas fa-shoe-prints"></i><b> {props.co2.value}</b> of CO2/{props.co2.unit} trip</div>}
+            modal nested >
+                <div className="row main">
                     {
                         props.c.map((e)=>{
                             return(
-                                <div>
-                                    
+                               
+                                <div className="col">
+                                    <div className="row value" >{e.value}</div>
+                                    <div className="row">{e.description}</div>
                                 </div>
+                                    
+                                
                             )
                         })
                     }
